980f8e3bc9dcca5bd863dff5c020f69fe39a5ba6
zingmars <git@zingmars.com>
2015-11-24 09:42:14 +0200

    Updated the database dump, added some semantic HTML5 tags to post view.

--- db.sql
+++ db.sql
@@ -9,6 +9,72 @@ DROP DATABASE IF EXISTS `dankpress`;
 CREATE DATABASE `dankpress` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `dankpress`;
 
+DROP TABLE IF EXISTS `authtokens`;
+CREATE TABLE `authtokens` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `public_token` text NOT NULL,
+  `private_token` text NOT NULL,
+  `type` int(11) NOT NULL DEFAULT '0',
+  `state` int(1) NOT NULL DEFAULT '1',
+  `createdate` datetime NOT NULL,
+  `lastusedate` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `authtokens_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DELIMITER ;;
+
+CREATE TRIGGER `createdate_auth` BEFORE INSERT ON `authtokens` FOR EACH ROW
+SET NEW.createdate=NOW();;
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `postid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `comment` text NOT NULL,
+  `date` datetime NOT NULL,
+  `editdate` datetime NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `postid` (`postid`),
+  KEY `userid` (`userid`),
+  CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`postid`) REFERENCES `posts` (`id`),
+  CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DELIMITER ;;
+
+CREATE TRIGGER `comments_postdate` BEFORE INSERT ON `comments` FOR EACH ROW
+SET NEW.date=NOW();;
+
+DELIMITER ;
+
+DROP TABLE IF EXISTS `filedata`;
+CREATE TABLE `filedata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` int(11) NOT NULL,
+  `name` int(11) DEFAULT NULL,
+  `upload_date` datetime NOT NULL,
+  `lastedit_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `accessflags` int(11) DEFAULT '1',
+  `password` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DELIMITER ;;
+
+CREATE TRIGGER `file_uploaddate` BEFORE INSERT ON `filedata` FOR EACH ROW
+SET NEW.upload_date=NOW();;
+
+DELIMITER ;
+
 DROP TABLE IF EXISTS `pagedata`;
 CREATE TABLE `pagedata` (
   `pageid` int(11) NOT NULL,
@@ -26,28 +92,53 @@ CREATE TABLE `pages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `url` text NOT NULL,
+  `accessflags` int(11) DEFAULT '1',
+  `password` text,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+DROP TABLE IF EXISTS `picturedata`;
+CREATE TABLE `picturedata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filename` int(11) NOT NULL,
+  `name` int(11) DEFAULT NULL,
+  `upload_date` datetime NOT NULL,
+  `lastedit_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `accessflags` int(11) DEFAULT '1',
+  `password` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DELIMITER ;;
+
+CREATE TRIGGER `picture_uploaddate` BEFORE INSERT ON `picturedata` FOR EACH ROW
+SET NEW.upload_date=NOW();;
+
+DELIMITER ;
 
 DROP TABLE IF EXISTS `posts`;
 CREATE TABLE `posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text NOT NULL,
-  `shortbody` text NOT NULL,  
+  `shortbody` text NOT NULL,
   `longbody` text NOT NULL,
   `createdate` datetime NOT NULL,
   `lasteditdate` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  `creator` int(2) NOT NULL DEFAULT '1',
+  `creator` int(11) NOT NULL DEFAULT '1',
   `lasteditor` int(2) NOT NULL DEFAULT '1',
   `editreason` text NOT NULL,
   `showeditreason` tinyint(1) NOT NULL DEFAULT '0',
+  `commentsallowed` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `creator` (`creator`),
   KEY `lasteditor` (`lasteditor`),
   CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`),
-  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`lasteditor`) REFERENCES `users` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+  CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`lasteditor`) REFERENCES `users` (`uid`),
+  CONSTRAINT `posts_ibfk_3` FOREIGN KEY (`creator`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 DELIMITER ;;
 
@@ -75,6 +166,14 @@ SET NEW.date=NOW();;
 
 DELIMITER ;
 
+DROP TABLE IF EXISTS `translationstrings`;
+CREATE TABLE `translationstrings` (
+  `string` text NOT NULL,
+  `value` text NOT NULL,
+  UNIQUE KEY `string` (`string`(10))
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
@@ -90,7 +189,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `users` (`uid`, `username`, `password`, `registerdate`, `lastlogin`, `lastloginip`, `lastloginsession`, `privlvl`) VALUES
-(1,	'nulluser',	'no logins are allowed for this user',	'2015-08-27 13:38:20',	'2015-08-27 13:38:20',	'0.0.0.0',	'no sessions are allowed for this user',	0);
+(1, 'nulluser', 'no logins are allowed for this user',  '2015-08-27 13:38:20',  '2015-08-27 13:38:20',  '0.0.0.0',  'no sessions are allowed for this user',  0);
 
 DELIMITER ;;
 
--- src/Template/Posts/view.ctp
+++ src/Template/Posts/view.ctp
@@ -1,19 +1,24 @@
-<div class="view-post">
-    <h1><?=$post->title?></h1>
-    <p>Posted on: <?php
-        echo($post->createdate->format('Y/m/d H:i:s'));
-        if(!is_null($post->lasteditdate)) {
-            echo ", edited at ".$post->lasteditdate->format('Y/m/d H:i:s');
-            if($post->showeditreason !== false && $post->editreason !== "") {
-                echo ", reason - \"".$post->editreason."\"";} }
-        ?></p>
-    <p><?php echo($post->longbody) ?></p>
-    <br />
-    <?php ?>
+<article class="view-post hentry">
+    <header>
+        <h1 class="entry-title"><?=$post->title?></h1>
+        <h2 class="entry-summary" style="display: none;"></h2>
+        
+        <time class="published" datetime="<?php $post->createdate ?>">Posted on: <?php
+            echo($post->createdate->format('Y/m/d H:i:s'));
+            if(!is_null($post->lasteditdate)) {
+                echo ", edited at ".$post->lasteditdate->format('Y/m/d H:i:s');
+                if($post->showeditreason !== false && $post->editreason !== "") {
+                    echo ", reason - \"".$post->editreason."\"";} }
+            ?></time>
+    </header>
+    <div class="entry-content">
+        <p><?php echo($post->longbody) ?></p>
+        <br />
+    </div>
     <h2>Comments: </h2>
     <?php foreach ($comments as $comment): ?>
         <h4><?php echo $comment->comment ?></h4>
         <p>Posted by <?php echo $comment->username?> on <?php echo $comment->date->format('Y/m/d H:i:s');  if ($comment->editdate !== null) {echo " and edited on ".$comment->editdate->format('Y/m/d H:i:s');}?></p>
     <?php endforeach; ?>
     <a href="#" id="gobackbutton" onclick="window.history.back()">Go back</a>
-</div>
+</article>
